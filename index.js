5e90d13e1802d44bb5d2eaeae3b14c8ec5430d01
ThiagoRochaS <82243264+ThiagoRochaS@users.noreply.github.com>
2023-03-31 23:17:32 -0300

    Banco de Dados

--- index.js
+++ index.js
@@ -1,86 +1,89 @@
 const express = require('express')
+const db = require('./db/mysql')
 
 const app = express()
-
+ 
 app.use(express.json())
-
-const soCarroBala = [
-    {
-        marca: 'Honda',
-        modelo: 'NSX'
-    },
-    {
-        marca: 'Mazda',
-        modelo: 'RX7'
-    }
-]
-
-app.get('/', (req, res) => {
-    res.send('Sejá bem vindo á API de carros!')
-})
-
-
+ 
 // CRUD
-
+ 
 //CREATE
+
 app.post('/api/carros', (req, res) => {
-    const carro = req.body
- 
-    soCarroBala.push(carro)
- 
-    console.log(soCarroBala[0])
- 
-    res.status(201).send();
+    try {
+        const carro = req.body
+        let sql = `INSERT INTO Carro (Marca, Modelo) VALUES ("${carro.marca}", "${carro.modelo}")`
+        db.query(sql, (err, result) => {
+            if(err) throw err
+            res.status(201).send({message: 'created'})
+        })
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
  
 // READ
 app.get('/api/carros', (req, res) => {
-    const carros = soCarroBala
- 
-    if (!carros.length > 0) return res.status(404).send('Não tem carros')
- 
-    return res.status(200).send(carros)
+    try {
+        let sql = `SELECT * FROM Carro`
+        db.query(sql, (err, result) => {
+            if(err) throw err
+            if (!result.length > 0) return res.status(404).send('Não tem carros')
+            res.status(200).send(result)
+        })
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
  
 app.get('/api/carros/:id', (req, res) => {
-    const id = req.params.id
- 
-    if (!soCarroBala.length > 0) return res.status(404).send('Não tem carros')
- 
-    if (id > soCarroBala.length - 1) return res.status(400).send('Não existem carros com esse id')
- 
-    const carroAchado = soCarroBala[id]
-    
-    return res.status(200).send(carroAchado)
+    try {
+        const id = req.params.id
+        let sql = `SELECT * FROM Carro WHERE ID = ${id}`
+        db.query(sql, (err, result) => {
+            if(err) throw err
+            if (!result.length > 0) return res.status(404).send('Carro não encontrado')
+            res.status(200).send(result)
+        })
+    } catch (error) {
+        res.status(500).send(error)
+    }
 })
  
  
 // UPDATE
 app.put('/api/carros/:id', (req, res) => {
-    const id = req.params.id
- 
-    if (!soCarroBala.length > 0) return res.status(404).send('Não tem carros')
- 
-    if (id > soCarroBala.length - 1) return res.status(400).send('Não existem carros com esse id')
- 
-    soCarroBala[id] = req.body     
-    
-    return res.status(204).send()
+    try {
+        const id = req.params.id
+        const carro = req.body
+        let sql = `UPDATE Carro SET Marca = "${carro.marca}", Modelo = "${carro.modelo}" WHERE ID = ${id}`
+        db.query(sql, (err, result) => {
+            if(err) throw err
+            res.status(200).send({message: 'updated'})
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+    }
 })
  
-// DESTROY
+// DESTROY  
 app.delete('/api/carros/:id', (req, res) => {
-    const id = req.params.id
- 
-    if (!soCarroBala.length > 0) return res.status(404).send('Não tem carros')
- 
-    if (id > soCarroBala.length - 1) return res.status(400).send('Não existem carros com esse id')
- 
-    const removido = soCarroBala.splice(id, 1)
-    
-    return res.status(200).send(removido)
+    try {
+        const id = req.params.id
+        const carro = req.body
+        let sql = `DELETE FROM Carro WHERE ID = ${id}`
+        db.query(sql, (err, result) => {
+            if(err) throw err
+            res.status(200).send({message: 'updated'})
+        })
+    } catch (error) {
+        console.log(error)
+        res.status(500).send(error)
+    }
 })
  
 app.listen(3000, () => {
     console.log('Server escutando na porta 3000')
 })
+
